094fea052f77402ffd4b7178ca84a07b878a6fac
Radu Constantin <raduadrian.constantin94@gmail.com>
2026-06-11 13:53:51 +0300

    Update backend security config to allow requests from the deployed frontend

--- src/main/java/com/thedevs/real_estate/security/SecurityConfig.java
+++ src/main/java/com/thedevs/real_estate/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "https://real-estate-frontend-gny1.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
